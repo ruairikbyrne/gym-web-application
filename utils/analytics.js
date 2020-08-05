a79de95f725d29a37f480e3d237dfa3a88d041be
Glitch (rural-animated-authorization) <none>
2020-08-05 16:12:18 +0000

    🐃📼 Checkpoint ./utils/analytics.js:17814122/655

--- utils/analytics.js
+++ utils/analytics.js
@@ -4,19 +4,13 @@ const analytics = {
   
     calculateBMI(request, response) {
     const loggedInUser = accounts.getCurrentUser(request);
-    const newAssessment = {
-      id: uuid.v1(),
-      userid: loggedInUser.id,
-      weight: request.body.weight,
-      chest: request.body.chest,
-      thigh: request.body.thigh,
-      upperArm: request.body.upperArm,
-      waist: request.body.waist,
-      hips: request.body.hips,
-    };
-    logger.debug('Creating a new Assessment', newAssessment);
-    memberAssessments.addAssessment(newAssessment);
-    response.redirect('/dashboard');
+    if (loggedInUser.height <= 0){
+      return 0;
+    }
+    else {
+      return (loggedInUser.weight / (loggedInUser.height * loggedInUser.height));
+    }
+    
   },
 
 };
