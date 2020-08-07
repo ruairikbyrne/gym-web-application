4ba6507212b545556cd57837d15292354c852c6b
Glitch (rural-animated-authorization) <none>
2020-08-07 16:29:57 +0000

    👧🚖 Checkpoint ./utils/analytics.js:17814122/302 ./models/user-store.json:17814122/10 ./views/partials/analytics.hbs:17814122/376

--- utils/analytics.js
+++ utils/analytics.js
@@ -8,7 +8,7 @@ const analytics = {
   
 
   
-    calculateBMI(request) {
+    calculateBMI(request, response) {
     var memberBMI = 0;  
     const loggedInUser = accounts.getCurrentUser(request);
     if (loggedInUser.height <= 0){
@@ -18,7 +18,9 @@ const analytics = {
     else {
       logger.info("calculating BMI: " + loggedInUser.name);
       //return conversion.round((loggedInUser.weight / (loggedInUser.height * loggedInUser.height)), 2);
-      memberBMI = (Number(loggedInUser.weight) / (Number(loggedInUser.height) * Number(loggedInUser.height)));
+      //memberBMI = (Number(loggedInUser.weight) / (Number(loggedInUser.height) * Number(loggedInUser.height)));
+      memberBMI = (Number(loggedInUser.startingWeight) / (Number(loggedInUser.height) * Number(loggedInUser.height)));
+      logger.info("calculated BMI: " + memberBMI);
       return memberBMI;
     }
     
