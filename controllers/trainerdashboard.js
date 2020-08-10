510cce3c84c03fe773ed8afa2fdfb041f792c11b
Glitch (rural-animated-authorization) <none>
2020-08-10 16:30:09 +0000

    ⏳🎾 Checkpoint ./views/trainerdashboard.hbs:17814122/72 ./views/partials/member-table.hbs:17814122/734 ./views/partials/member-header.hbs:17814122/208 ./controllers/trainerdashboard.js:17814122/1119

--- controllers/trainerdashboard.js
+++ controllers/trainerdashboard.js
@@ -10,37 +10,11 @@ const uuid = require('uuid');
 const trainerdashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
-    const loggedInUser = accounts.getCurrentUser(request);
-    //const memberBMI = analytics.calculateBMI(request);
-    //logger.info("calculated bmi " + memberBMI);
     const viewData = {
-      memberName: loggedInUser.name,
-      memberBMI: analytics.calculateBMI(request),
-      categoryBMI: analytics.determineBMICategory(request),
-      idealWeight: analytics.isIdealBodyWeight(request),
-      assessments: memberAssessments.getUserAssessments(loggedInUser.id),
+      title: 'Member',
     };
-    logger.info("about to render ", memberAssessments.getAllAssessments());
-    response.render("dashboard", viewData);
-  },
-
-
-
-  addAssessment(request, response) {
-    const loggedInUser = accounts.getCurrentUser(request);
-    const newAssessment = {
-      id: uuid.v1(),
-      userid: loggedInUser.id,
-      weight: request.body.weight,
-      chest: request.body.chest,
-      thigh: request.body.thigh,
-      upperArm: request.body.upperArm,
-      waist: request.body.waist,
-      hips: request.body.hips,
-    };
-    logger.debug('Creating a new Assessment', newAssessment);
-    memberAssessments.addAssessment(newAssessment);
-    response.redirect('/dashboard');
+    
+    response.render("trainerdashboard", viewData);
   },
 
 };
